7cbe78a678f77936223fec8df6e9f963efef947e
Danila <danil.lysunckin@gmail.com>
2024-11-13 00:32:59 +0500

    ImageService gets path for saving and taking from property file

--- src/main/java/success/navigator/website/services/ImageService.java
+++ src/main/java/success/navigator/website/services/ImageService.java
@@ -1,6 +1,7 @@
 package success.navigator.website.services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,16 +17,17 @@ import java.util.List;
 @Service
 @Slf4j
 public class ImageService {
-    public final String STATIC_DIR = File.separator + "opt" + File.separator + "navigator" + File.separator + "images";
+    @Value("${images-path}")
+    public String images_path;
 
     public List<String> getStaticImages() {
-        File[] file = new File(STATIC_DIR).listFiles();
+        File[] file = new File(images_path).listFiles();
         return file == null ? new ArrayList<>() : Arrays.stream(file).map(File::getName).toList();
     }
 
     public void addStaticImageToDir(MultipartFile multipartFile) {
         try {
-            multipartFile.transferTo(Path.of(STATIC_DIR + File.separator + multipartFile.getOriginalFilename()));
+            multipartFile.transferTo(Path.of(images_path + File.separator + multipartFile.getOriginalFilename()));
         } catch (IOException e) {
             log.error("Unable to add image file to static files dir");
             log.error(e.toString());
@@ -34,7 +36,7 @@ public class ImageService {
 
     public void deleteStaticImageFromDir(@PathVariable String image) {
         try {
-            Files.delete(Path.of(STATIC_DIR + File.separator + image));
+            Files.delete(Path.of(images_path + File.separator + image));
         } catch (IOException e) {
             log.error("Unable to delete image in static files dir");
             log.error(e.toString());
